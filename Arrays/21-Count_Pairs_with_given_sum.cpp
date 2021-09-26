add1843ead67d3ddcdb3ff64752d1dd1a81a4db6
Akshay Varshney <akshayvarshney.001@gmail.com>
2021-09-26 14:13:20 +0530

    Common Element and Pair Count

---
+++ Arrays/21-Count_Pairs_with_given_sum.cpp
@@ -0,0 +1,53 @@
+// Array: Count pairs with given sum
+
+// https://practice.geeksforgeeks.org/problems/count-pairs-with-given-sum5022/1#
+/*
+ Input:
+ N = 4, K = 6
+ arr[] = {1, 5, 7, 1}
+ Output: 2
+*/
+
+#include<vector>
+#include<iostream>
+#include<unordered_map>
+
+using namespace std;
+// Using Unordered map to store the count of the variable.
+int getpair_count(vector<int> A,int n, int x){
+    unordered_map<int, int> m1;
+    for(int i=0;i<n;i++){
+        std::unordered_map<int, int>::iterator it = m1.find(A[i]);
+           if (it != m1.end()) {
+               it->second++;
+           }
+           else {
+               m1.insert(std::make_pair(A[i], 1));
+           }
+    }
+    int count=0;
+    bool flag=false;
+    for(int i=0;i<n;i++){
+        flag=false;
+        std::unordered_map<int, int>::iterator it = m1.find(x-A[i]);
+        
+           if (it != m1.end()) {
+               if(A[i]==it->first){
+                   it->second--;
+                   flag=true;
+               }
+               count+=it->second;
+           }
+        if(flag==true){
+            it->second++;
+        }
+    }
+    return count/2;
+}
+// Time : O(n) and Space: O(n)
+
+int main(){
+    vector<int> v1={1,1, 1, 1};
+    vector<int> v2;
+    cout<<getpair_count(v1, v1.size(), 2);
+}
---
+++ Arrays/22-Common_Element_in_array.cpp
@@ -0,0 +1,52 @@
+// Array: Common Elements in three sorted Arrays
+
+// https://practice.geeksforgeeks.org/problems/common-elements1132/1#
+/*
+ Input:
+ n1 = 6; A = {1, 5, 10, 20, 40, 80}
+ n2 = 5; B = {6, 7, 20, 80, 100}
+ n3 = 8; C = {3, 4, 15, 20, 30, 70, 80, 120}
+ Output: 20 80
+
+*/
+#include<vector>
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+vector<int> _common_element(vector<int> A, vector<int> B, vector<int> C, int n1, int n2, int n3){
+    if(n1==0 || n2==0 || n3==0) return {};
+    int i=0,j=0, k=0;
+    vector<int> v;
+    while(i<n1 && j<n2 && k<n3){
+        if(A[i]==B[j] && B[j]==C[k]){
+            auto it=v.end()-1;
+            if(v.size()>0 && *it==A[i] ){
+                i++;
+                j++;
+                k++;
+                continue;
+            }
+            v.push_back(A[i]);
+            i++;
+            j++;
+            k++;
+        }
+        else if(A[i]<B[j]) i++;
+        else if(B[j]<C[k]) j++;
+        else k++;
+    }
+    return v;
+}
+
+int main(){
+    vector<int> v1={1, 5, 10, 20, 40, 80};
+    vector<int> v2={6, 7, 20, 80, 100};
+    vector<int> v3={3, 4, 15, 20, 30, 70, 80, 120};
+
+    vector<int> v4;
+    v4=_common_element(v1, v2, v3, v1.size(), v2.size(), v3.size());
+    for(auto it=v4.begin();it!=v4.end();it++){
+        cout<<*it<<" ";
+    }
+  
+}

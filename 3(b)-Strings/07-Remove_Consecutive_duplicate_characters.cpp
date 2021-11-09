93ee9ce4d84b5b1c5ea5666e417c585829a08b88
Akshay Varshney <akshayvarshney.001@gmail.com>
2021-11-09 09:24:46 +0530

    Remove Adjacent duplicates by recursive and non recursive way

--- 3(b)-Strings/07-Remove_Consecutive_duplicate_characters.cpp
+++ 3(b)-Strings/07-Remove_Consecutive_duplicate_characters.cpp
@@ -25,8 +25,25 @@ string remove_dupliate(string A){
 }
 //Time: O(n) and Space: O(1)
 
+// Methgod-2 by using recurssion
+string removeConsecutive(string s)
+   {
+       int n = s.size();
+       if(n == 0 || n == 1){
+           return s;
+       }
+       if(s[0] == s[1]){
+           int index = 0;
+           while(s[index] == s[0] && index < n){
+               index++;
+           }
+           return s[0] + removeConsecutive(s.substr(index));
+       }
+       return s[0] + removeConsecutive(s.substr(1));
+   }
 
 int main(){
     string A="aabaa";
     cout<<remove_dupliate(A)<<endl;
+    cout<<removeConsecutive(A)<<endl;
 }

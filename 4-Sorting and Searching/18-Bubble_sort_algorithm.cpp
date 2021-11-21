29b639963cbe25ebba3736c1a2a328f1ba813927
Akshay Varshney <akshayvarshney.001@gmail.com>
2021-11-21 10:56:26 +0530

    Bubble Sort and Recursive Bubble Sort

---
+++ 4-Sorting and Searching/18-Bubble_sort_algorithm.cpp
@@ -0,0 +1,52 @@
+// Searching & Sorting: Bubble Sort Algorithm
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void bubblesort(vector<int> &A){
+    bool flag=false;
+    int n=A.size();
+    for(int i=0;i<n-1;i++){
+        flag=false;
+        for(int j=0;j<n-i-1;j++){
+            if(A[j]>A[j+1]){
+                swap(A[j+1],A[j]);
+                flag=true;
+            }
+            if(flag==false){
+                break;
+            }
+        }
+    }
+}
+
+//Time: O(n2)
+
+//Method 2: By using Recursion
+void recursive_bubble_sort(vector<int> &A, int n){
+    if(n==1){
+        return;
+    }
+    for(int i=0;i<n-1;i++){
+        if(A[i]>A[i+1]){
+            swap(A[i], A[i+1]);
+        }
+    }
+    recursive_bubble_sort(A, n-1);
+}
+
+int main(){
+    vector<int> A={19,1,4,2,8,9};
+    vector<int> B={19,1,4,2,8,9};
+    bubblesort(A);
+    recursive_bubble_sort(B, B.size());
+    for(int i=0;i<A.size();i++){
+        cout<<A[i]<<" ";
+    }
+    cout<<endl;
+    for(int i=0;i<B.size();i++){
+        cout<<B[i]<<" ";
+    }
+    cout<<endl;
+}

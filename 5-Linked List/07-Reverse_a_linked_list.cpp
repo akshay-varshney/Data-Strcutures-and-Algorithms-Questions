3eb75b5a2e3ac7d9ee8d0faa5f5e74dc28b276f9
Akshay Varshney <akshayvarshney.001@gmail.com>
2021-11-27 22:32:58 +0530

    Reverse a linked list

---
+++ 5-Linked List/07-Reverse_a_linked_list.cpp
@@ -0,0 +1,53 @@
+// Linked List: Reverse a Linked List
+#include <iostream>
+
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node *next;
+};
+void Print_Linked_list(Node *n){
+    while(n!=nullptr){
+        cout<<n->data<<"->";
+        n=n->next;
+    }
+    cout<<endl;
+}
+// Insertion of node at the start of linked list. Updating the address of the head pointer to the new head.
+Node* Insert_start(Node *head, int data){
+    Node* temp = new Node();
+    temp->data=data;
+           temp->next = head;
+           head = temp;
+    return head;
+}
+// Linked List reversal by keeping track of the previous, current and next pointers
+Node * reverse_linked_list(Node *head){
+    Node *prev=new Node();
+    Node *Next=new Node();
+    prev=NULL;
+    Node *temp=new Node();
+    temp=head;
+    while(temp!=NULL){
+        Next=temp->next;
+        temp->next=prev;
+        prev=temp;
+        temp=Next;
+    }
+    head=prev;
+    return  head;
+}
+
+
+int main(int argc, const char * argv[]) {
+    Node *head=new Node();
+    head=NULL;
+    head=Insert_start(head, 1);
+    head=Insert_start(head, 12);
+    head=Insert_start(head, 14);
+    Print_Linked_list(head);
+    head=reverse_linked_list(head);
+    Print_Linked_list(head);
+}

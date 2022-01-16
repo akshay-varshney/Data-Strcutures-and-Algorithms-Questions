028c516ce2f29be36f5d21ffe09a28461027bb8a
Akshay Varshney <akshayvarshney.001@gmail.com>
2022-01-16 22:05:07 +0530

    Inorder Tree Traversal using Iterative and Recursive Ways.

---
+++ 8-Binary Trees/07-Inorder_Traversal_of_tree.cpp
@@ -0,0 +1,74 @@
+//Binary Tree: Inorder traversal of Tree using Iterative and Recursive way
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class Node {
+    
+    public:
+        int data;
+        Node* left;
+        Node* right;
+        Node *CreateNode(int data);
+        Node *InsertNode(Node* root, int data);
+        void inorder(Node* root);
+        void recursive_inorder(Node* root);
+};
+
+Node* Node::CreateNode(int data)
+{
+    Node* newNode = new Node();
+    if (!newNode) {
+        return NULL;
+    }
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+void Node::inorder(Node* root)
+{
+    if (root == NULL)
+        return;
+    inorder(root->left);
+    cout <<" "<< root->data;
+    inorder(root->right);
+}
+
+// using stack to get the last elemnt
+void Node::recursive_inorder(Node *root){
+    if(root==NULL){
+        return;
+    }
+    stack<Node*> S1;
+    Node* temp=root;
+    while (!S1.empty() || temp!=NULL) {
+        while(temp!=NULL){
+            S1.push(temp);
+            temp=temp->left;
+        }
+        temp=S1.top();
+        S1.pop();
+        cout<<temp->data<<" ";
+        temp=temp->right;
+    }
+}
+    
+
+
+int main()
+{
+    Node BT;
+    Node* root = BT.CreateNode(1);
+    root->left = BT.CreateNode(2);
+    root->right = BT.CreateNode(3);
+    root->left->left = BT.CreateNode(4);
+    root->right->left = BT.CreateNode(5);
+    root->right->right = BT.CreateNode(6);
+    root->right->right->left = BT.CreateNode(9);
+    cout <<"Inorder of original tree: ";
+    BT.inorder(root);
+    cout<<endl;
+    cout<<"Recursive Inorder Traversal: "<<endl;
+    BT.recursive_inorder(root);
+}

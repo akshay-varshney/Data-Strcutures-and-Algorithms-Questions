cf1af0179014badd2ea6a8f6d1efb2072448130d
Akshay Varshney <akshayvarshney.001@gmail.com>
2022-02-18 21:10:35 +0530

    Update 01-Adjacency_LIst_Representation_of_Graph.cpp

--- 11-Graphs/01-Adjacency_LIst_Representation_of_Graph.cpp
+++ 11-Graphs/01-Adjacency_LIst_Representation_of_Graph.cpp
@@ -8,35 +8,37 @@ using namespace std;
 
 // A utility function to add an edge in an
 // undirected graph.
-void addEdge(vector<int> A[], int start, int end)
+void addEdge(vector<int> adj[], int u, int v)
 {
-    A[start].push_back(end);
-    A[end].push_back(start);
+    adj[u].push_back(v);
+    adj[v].push_back(u);
 }
 
-
+// A utility function to print the adjacency list
+// representation of graph
 void printGraph(vector<int> adj[], int V)
 {
     for (int v = 0; v < V; ++v) {
         cout << "\n Adjacency list of vertex " << v
-             << "\n head ";
+            << "\n head ";
         for (auto x : adj[v])
             cout << "-> " << x;
-        cout<<endl;
+        printf("\n");
     }
 }
 
+// Driver code
 int main()
 {
-    int end = 5;
-    vector<int> A[end];
-    addEdge(A, 0, 1);
-    addEdge(A, 0, 4);
-    addEdge(A, 1, 2);
-    addEdge(A, 1, 3);
-    addEdge(A, 1, 4);
-    addEdge(A, 2, 3);
-    addEdge(A, 3, 4);
-    printGraph(A, end);
+    int V = 5;
+    vector<int> adj[V];
+    addEdge(adj, 0, 1);
+    addEdge(adj, 0, 4);
+    addEdge(adj, 1, 2);
+    addEdge(adj, 1, 13);
+    addEdge(adj, 1, 4);
+    addEdge(adj, 2, 13);
+    addEdge(adj, 13, 4);
+    printGraph(adj, V);
     return 0;
 }

d8a3aa172f0175db855d39c25a62d88970292617
Akshay Varshney <akshayvarshney.001@gmail.com>
2022-03-13 00:12:28 +0530

    0/1 Knapsack using Memoization and Top Down

--- 13-Dynamic Programming/02-0:1_Knapsack.cpp
+++ 13-Dynamic Programming/02-0:1_Knapsack.cpp
@@ -1,9 +1,14 @@
-// Dynamic Programming: Fibonaci Example of what is dynamic Programming.
+// Dynamic Programming: 0/1 Knapsack
 /*
- condition to apply DP:
-1.  Optimal Substructure: by solving sub problems we should be able to get the result of the entire problem.
- 2. Repeating sub problems.
+ Input:
+ Weights: { 2, 3, 1, 4 }
+ Profits: { 4, 5, 3, 7 }
+ Knapsack capacity: 5
+ 
+ O/p: 10
  */
+// https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/
+
 
 #include<iostream>
 #include<vector>
@@ -12,6 +17,8 @@ using namespace std;
 //int max(int a, int b) { return (a > b) ? a : b; }
 
 // Method 1: Normal recurssion.
+// recursive call after choosing the element at the currentIndex
+// if the weight of the element at currentIndex exceeds the capacity, we shouldn't process this
 int knapScak(int w, vector<int>& wt, vector<int>& profit, int n){
     if(n==0 || w==0)
         return 0;
@@ -20,8 +27,12 @@ int knapScak(int w, vector<int>& wt, vector<int>& profit, int n){
     else
         return max((knapScak(w, wt, profit, n-1)), (profit[n]+knapScak(w-wt[n], wt, profit, n-1)));
 }
+// Time: 2^n
 
+
+// Method 2: Dynamic Programming with Memoization
 int MemoizationknapScak(int w, vector<int>& wt, vector<int>& profit, int n, vector<vector<int>>& dp){
+    //if we have already solved a similar problem, return the result from memory
     if(dp[n][w]!=-1)
         return dp[n][w];
     if(n==0 || w==0)
@@ -30,6 +41,7 @@ int MemoizationknapScak(int w, vector<int>& wt, vector<int>& profit, int n, vect
         dp[n][w]=MemoizationknapScak(w, wt, profit, n-1, dp);
         return dp[n][w];
     }
+    //recursive call after excluding the element at the currentIndex
     else{
         dp[n][w]= max((MemoizationknapScak(w, wt, profit, n-1, dp)), (profit[n]+MemoizationknapScak(w-wt[n], wt, profit, n-1, dp)));
         return dp[n][w];
@@ -37,6 +49,7 @@ int MemoizationknapScak(int w, vector<int>& wt, vector<int>& profit, int n, vect
 
 }
 
+// Method 3: Dynamic Programming with Tabulation
 int tabulationKnapsack(int w, vector<int>& wt, vector<int>& profit, int n, vector<vector<int>>& dp){
     for(int i=0;i<n;i++){
         for(int j=0;j<w;j++){
@@ -48,7 +61,7 @@ int tabulationKnapsack(int w, vector<int>& wt, vector<int>& profit, int n, vecto
                 dp[i][j]= max(dp[i-1][j], profit[i-1]+dp[i-1][w-wt[i-1]]);
         }
     }
-    return dp[n][w];
+    return dp[n-1][w];
 }
 
 
@@ -57,7 +70,7 @@ int main() {
     vector<int> profit= {6,8,7};
     int w=8;
     int n=profit.size()+1;
-    cout<<knapScak(w, wt, profit, n-1)<<endl;
+    cout<<"Maximum Profit is: "<<knapScak(w, wt, profit, n-1)<<endl;
     vector<vector<int>> dp(n, vector<int> (w+1));
     for(int i=0; i<n; i++){
         for(int j=0;j<w+1;j++){
@@ -65,6 +78,7 @@ int main() {
         }
     }
     
-    cout<<MemoizationknapScak(w, wt, profit, wt.size()-1, dp)<<endl;
-    cout<<tabulationKnapsack(w, wt, profit, wt.size()-1, dp)<<endl;
+    cout<<"Maximum Profit is: "<<MemoizationknapScak(w, wt, profit, wt.size()-1, dp)<<endl;
+    cout<<"Maximum Profit is: "<<tabulationKnapsack(w, wt, profit, wt.size(), dp)<<endl;
+    dp.clear();
 }

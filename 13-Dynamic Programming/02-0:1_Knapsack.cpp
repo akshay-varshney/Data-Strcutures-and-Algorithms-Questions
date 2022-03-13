253950144d0adff2fea762fa3fa680103121ce46
Akshay Varshney <akshayvarshney.001@gmail.com>
2022-03-13 20:42:17 +0530

    Update 02-0:1_Knapsack.cpp

--- 13-Dynamic Programming/02-0:1_Knapsack.cpp
+++ 13-Dynamic Programming/02-0:1_Knapsack.cpp
@@ -1,3 +1,5 @@
+
+
 // Dynamic Programming: 0/1 Knapsack
 /*
  Input:
@@ -22,10 +24,10 @@ using namespace std;
 int knapScak(int w, vector<int>& wt, vector<int>& profit, int n){
     if(n==0 || w==0)
         return 0;
-    if(wt[n]> w)
+    if(wt[n-1]> w)
         return knapScak(w, wt, profit, n-1);
     else
-        return max((knapScak(w, wt, profit, n-1)), (profit[n]+knapScak(w-wt[n], wt, profit, n-1)));
+        return max((knapScak(w, wt, profit, n-1)), (profit[n-1]+knapScak(w-wt[n-1], wt, profit, n-1)));
 }
 // Time: 2^n
 
@@ -33,10 +35,11 @@ int knapScak(int w, vector<int>& wt, vector<int>& profit, int n){
 // Method 2: Dynamic Programming with Memoization
 int MemoizationknapScak(int w, vector<int>& wt, vector<int>& profit, int n, vector<vector<int>>& dp){
     //if we have already solved a similar problem, return the result from memory
+    if(n<0)
+        return 0;
+    
     if(dp[n][w]!=-1)
         return dp[n][w];
-    if(n==0 || w==0)
-        return 0;
     if(wt[n]> w){
         dp[n][w]=MemoizationknapScak(w, wt, profit, n-1, dp);
         return dp[n][w];
@@ -51,34 +54,42 @@ int MemoizationknapScak(int w, vector<int>& wt, vector<int>& profit, int n, vect
 
 // Method 3: Dynamic Programming with Tabulation
 int tabulationKnapsack(int w, vector<int>& wt, vector<int>& profit, int n, vector<vector<int>>& dp){
-    for(int i=0;i<n;i++){
-        for(int j=0;j<w;j++){
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=w;j++){
             if(i==0 || j==0)
                 dp[i][j]=0;
             else if(wt[i-1]>j)
                 dp[i][j]=dp[i-1][j];
             else
-                dp[i][j]= max(dp[i-1][j], profit[i-1]+dp[i-1][w-wt[i-1]]);
+                dp[i][j]= max(dp[i-1][j], profit[i-1]+dp[i-1][j-wt[i-1]]);
         }
     }
-    return dp[n-1][w];
+    return dp[n][w];
 }
 
 
 int main() {
-    vector<int> wt={3,2,4};
-    vector<int> profit= {6,8,7};
-    int w=8;
-    int n=profit.size()+1;
-    cout<<"Maximum Profit is: "<<knapScak(w, wt, profit, n-1)<<endl;
-    vector<vector<int>> dp(n, vector<int> (w+1));
-    for(int i=0; i<n; i++){
+    vector<int> wt={10,20,30};
+    vector<int> profit= {60,100,120};
+    int w=50;
+    int n=profit.size();
+    cout<<"Maximum Profit is: "<<knapScak(w, wt, profit, n)<<endl;
+    vector<vector<int>> dp(n+1, vector<int> (w+1));
+    for(int i=0; i<n+1; i++){
         for(int j=0;j<w+1;j++){
             dp[i][j]=-1;
         }
     }
     
-    cout<<"Maximum Profit is: "<<MemoizationknapScak(w, wt, profit, wt.size()-1, dp)<<endl;
-    cout<<"Maximum Profit is: "<<tabulationKnapsack(w, wt, profit, wt.size(), dp)<<endl;
+    cout<<"Maximum Profit is: "<<MemoizationknapScak(w, wt, profit, n-1, dp)<<endl;
     dp.clear();
+    vector<vector<int>> dp2(n+1, vector<int> (w+1));
+    for(int i=0; i<n+1; i++){
+        for(int j=0;j<w+1;j++){
+            dp2[i][j]=-1;
+        }
+    }
+
+    cout<<"Maximum Profit is: "<<tabulationKnapsack(w, wt, profit, n, dp2)<<endl;
+    dp2.clear();
 }

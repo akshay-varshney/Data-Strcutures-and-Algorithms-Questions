eb567b1f73d7223b2dad2915198c24f7e674c24a
Akshay Varshney <akshayvarshney.001@gmail.com>
2022-03-27 08:58:11 +0530

    Palindrome Partitioning Memoized Optimisation

--- 13-Dynamic Programming/20-Pallindromic_Partitioning.cpp
+++ 13-Dynamic Programming/20-Pallindromic_Partitioning.cpp
@@ -1,5 +1,7 @@
 // Dynamic Programming: Palindrome Partitioning
 //https://www.geeksforgeeks.org/palindrome-partitioning-dp-17/
+// https://youtu.be/9h10fqkI7Nk?list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go
+
 
 #include<iostream>
 #include<vector>
@@ -50,6 +52,34 @@ int Memoizationpalindromicpartition(string A, int i, int j, vector<vector<int>>&
     return dp[i][j]=mini;
 }
 
+// Method 3: By using Optimized Memoization
+int optimizedMemoizationpalindromicpartition(string A, int i, int j, vector<vector<int>>& dp){
+    if(i>=j)
+        return 0;
+    if(isPlaindrome(A, i, j) )
+        return 0;
+    if(dp[i][j]!=-1)
+        return dp[i][j];
+    int mini= INT_MAX;
+    for(int k=i;k<j; k++){
+        int left, right;
+        if(dp[i][k]!=-1)
+            left=dp[i][k];
+        else{
+            left=optimizedMemoizationpalindromicpartition(A, i, k, dp);
+            dp[i][k]=left;
+        }
+        if(dp[k+1][j]!=-1)
+            right=dp[k+1][j];
+        else{
+            right=optimizedMemoizationpalindromicpartition(A, k+1, j, dp);
+            dp[k+1][j]=right;
+        }
+        int temp= left+ right + 1;
+        mini=min(mini,temp);
+    }
+    return dp[i][j]=mini;
+}
 int main(){
     string A= "ababbbabbababa";
     int n=A.size();
@@ -62,7 +92,15 @@ int main(){
         }
     }
     cout<<Memoizationpalindromicpartition(A,0,n-1,dp)<<endl;
+    vector<vector<int>> dp2(n+1, vector<int> (n+1));
+    for(int i=0; i<=n; i++){
+        for(int j=0;j<=n;j++){
+            dp2[i][j]=-1;
+        }
+    }
+    cout<<optimizedMemoizationpalindromicpartition(A, 0, n-1, dp2)<<endl;
     dp.clear();
+    dp2.clear();
 }
 
 

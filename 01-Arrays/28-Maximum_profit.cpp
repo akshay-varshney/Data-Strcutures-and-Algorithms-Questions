05544ca5fe2b7880f19c517b46bdc8059b513e7a
Akshay Varshney <akshayvarshney.001@gmail.com>
2022-04-20 22:11:54 +0530

    Maximum price (Updated)

--- 01-Arrays/28-Maximum_profit.cpp
+++ 01-Arrays/28-Maximum_profit.cpp
@@ -1,5 +1,6 @@
 // Array: Maximum profit by buying and selling a share at most twice
 // https://www.geeksforgeeks.org/maximum-profit-by-buying-and-selling-a-share-at-most-twice/
+
 #include<iostream>
 #include<vector>
 
@@ -11,32 +12,38 @@ int maximum_profit(vector<int> A){
     int mini2=A[1];
     int ind1=0;
     int ind2=1;
-    //finding the first and second minimum in array
-    for(int i=1;i<n;i++){
-        if(A[i]<mini){
-            if(mini2>mini){
+    //finding the first and second minimum in array and capturing their indices
+    for(int i=1;i<n;i++)
+    {
+        if(A[i]<mini)
+        {
+            if(mini2>mini)]
+            {
                 mini2=mini;
                 ind2=ind1;
             }
             mini=A[i];
             ind1=i;
         }
-        else if(A[i]<mini2){
+        else if(A[i]<mini2)
+        {
             mini2=A[i];
             ind2=i;
         }
     }
+    // Traversing two loops from  mini1 to Mini2-1 and second from Mini2 to n-1 and finding out the maximum profit in this range.
     int val1=max(ind1,ind2);
     int val2=min(ind1,ind2);
     int profit1=0;
-    for(int i=val2;i<val1-1;i++){
+    for(int i=val2;i<val1-1;i++)
         profit1=max(profit1,A[i+1]-A[val2]);
-    }
+    
     int summed=profit1;
     profit1=0;
-    for(int i=val1;i<n-1;i++){
+    
+    for(int i=val1;i<n-1;i++)
         profit1=max(profit1,A[i+1]-A[val1]);
-    }
+    
     summed+=profit1;
     return summed;
 }

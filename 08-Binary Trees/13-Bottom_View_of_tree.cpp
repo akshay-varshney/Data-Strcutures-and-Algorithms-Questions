97cb60a6099af4bbf1f633cc9e20fdf5879a833d
Akshay Varshney <akshayvarshney.001@gmail.com>
2022-05-15 08:39:36 +0530

    Update 13-Bottom_View_of_tree.cpp

--- 08-Binary Trees/13-Bottom_View_of_tree.cpp
+++ 08-Binary Trees/13-Bottom_View_of_tree.cpp
@@ -20,6 +20,7 @@ class Node {
         Node *CreateNode(int data);
         Node *InsertNode(Node* root, int data);
         vector<int> bottomView(Node* root);
+    void printBottomViewMethod2(Node * root, int curr, int hd, map <int, pair <int, int>> & m);
 };
 
 Node* Node::CreateNode(int data)
@@ -64,6 +65,39 @@ vector<int> Node::bottomView(Node *root)
     return ans;
 }
 
+
+void printBottomViewMethod2(Node * root, int curr, int hd, map <int, pair <int, int>> & m)
+{
+    // Base case
+    if (root == NULL)
+        return;
+     
+    // If node for a particular
+    // horizontal distance is not
+    // present, add to the map.
+    if (m.find(hd) == m.end())
+    {
+        m[hd] = make_pair(root -> data, curr);
+    }
+    // Compare height for already
+    // present node at similar horizontal
+    // distance
+    else
+    {
+        pair < int, int > p = m[hd];
+        if (p.second <= curr)
+        {
+            m[hd].second = curr;
+            m[hd].first = root -> data;
+        }
+    }
+     
+    // Recur for left subtree
+    printBottomViewUtil(root -> left, curr + 1, hd - 1, m);
+     
+    // Recur for right subtree
+    printBottomViewUtil(root -> right, curr + 1, hd + 1, m);
+}
 
 int main()
 {
